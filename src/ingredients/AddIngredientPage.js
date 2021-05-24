bd604f775d661c49c4b6e1d9281c8ddd34df17a9
Tadeo Palafox <palafot@miamioh.edu>
2021-05-23 23:31:42 -0400

    Created and set up project

---
+++ src/ingredients/AddIngredientPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AddIngredientPage = () => (
+    <h1>This is the Add Ingredient Page</h1>
+);
---
+++ src/ui/SmallX.js
@@ -0,0 +1,8 @@
+  
+import React from 'react';
+
+export const SmallX = ({ onClick }) => (
+    <div className="small-x">
+        <span className="the-x" onClick={onClick}>x</span>
+    </div>
+);
