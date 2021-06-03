a5619aa901505a86ee2918d7874c1412a18c293f
Tadeo Palafox <palafot@miamioh.edu>
2021-06-03 15:14:55 -0400

    Implemented Recipe search page

--- src/recipes/RecipeSearchPage.js
+++ src/recipes/RecipeSearchPage.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BackButton } from '../ui';
+import { useIngredients } from '../ingredients';
+import { RecipeSearchResultsList } from './RecipeSearchResultsList';
+import { useRecipeSearchResults } from './useRecipeSearchResults';
 
-export const RecipeSearchPage = () => (
-    <h1>This is the Recipe Search Page</h1>
-);
+export const RecipeSearchPage = () => {
+    const [searchString, setSearchString] = useState('');
+    const [searchInputValue, setSearchInputValue] = useState('');
+    const { ingredients } = useIngredients();
+    const { searchResults } = useRecipeSearchResults(searchString);
+
+    const onSearchClicked = () => {
+        setSearchString(searchInputValue);
+    }
+
+    return (
+        <div className="page">
+            <BackButton />
+            <div className="centered-container">
+                <h1>Add Meal to Plan</h1>
+                <input
+                    type="text"
+                    className="full-width space-before space-after"
+                    placeholder="Enter keyword here"
+                    value={searchInputValue}
+                    onChange={e => setSearchInputValue(e.target.value)} />
+                <button
+                    className="full-width space-after"
+                    onClick={onSearchClicked}
+                >Search</button>
+                <RecipeSearchResultsList
+                    recipes={searchResults}
+                    ingredients={ingredients} />
+            </div>
+        </div>
+    );
+}
